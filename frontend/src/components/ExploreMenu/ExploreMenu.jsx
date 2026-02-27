2ed148bdb59c6d02ecd9f289534504982b2f9aba
BigShanJohn <segun.adeniji01@gmail.com>
2026-02-27 03:48:27 +0000

    Task2 feat(ui): add hover zoom effect to menu images and fix explore-menu-text styling

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -7,12 +7,14 @@ const ExploreMenu = ({category, setCategory}) => {
   return (
     <div className='explore-menu' id='explore-menu'>
         <h1>Explore Our Menu</h1>
-        <p className='explore-menu=text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, exercitationem excepturi temporibus obcaecati voluptas vitae?</p>
+        <p className='explore-menu-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, exercitationem excepturi temporibus obcaecati voluptas vitae?</p>
         <div className="explore-menu-list">
             {menu_list.map((item,index)=>{
                 return (
                     <div onClick={()=>setCategory(prev=> prev === item.menu_name ? 'All' : item.menu_name)} key={index} className="explore-menu-list-item">
-                        <img className={category===item.menu_name?'active':''} src={item.menu_image} alt="" />
+                        <div className="menu-img-wrapper">
+                            <img className={category===item.menu_name?'active':''} src={item.menu_image} alt="" />
+                        </div>
                         <p>{item.menu_name}</p>
                     </div>
                 )
